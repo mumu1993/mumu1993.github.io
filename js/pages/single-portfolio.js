ebb2a3dd3f3b9d3921441f80b0bf42b6d9dc89b4
mumu1993 <mtrabc@vip.qq.com>
2016-07-09 19:41:21 +0800

    pages -v1.2.4 head hover

--- js/pages/single-portfolio.js
+++ js/pages/single-portfolio.js
@@ -1,60 +1,31 @@
 $(document).ready(function() {
-	//		$('.port-skills').waypoint(function() {
-	//		var html = Circles.create({
-	//			id : 'html_circle',
-	//			radius : 100,
-	//			value : 100,
-	//			width : 5,
-	//			colors : [ '#FCE6A4', '#EFB917' ],
-	//			text : function(value) {
-	//				return value + '%';
-	//			},
-	//		});
-	//		var css = Circles.create({
-	//			id : 'css_circle',
-	//			radius : 100,
-	////			value : 100,
-	//			width : 5,
-	//			colors : [ '#BEE3F7', '#45AEEA' ],
-	////			text : function(value) {
-	////				return value + '%';
-	////			},
-	//		});
-	//		var js = Circles.create({
-	//			id : 'js_circle',
-	//			radius : 100,
-	////			value : 100,
-	//			width : 5,
-	//			colors : [ '#F8F9B6', '#D2D558' ],
-	////			text : function(value) {
-	////				return value + '%';
-	////			},
-	//		});
-	//		var jq = Circles.create({
-	//			id : 'jq_circle',
-	//			radius : 100,
-	////			value : 100,
-	//			width : 5,
-	//			colors : [ '#F4BCBF', '#D43A43' ],
-	////			text : function(value) {
-	////				return value + '%';
-	////			},
-	//		});
-	//	}, {
-	//		offset : '85%',
-	//		triggerOnce: true,
-	//	});
+	$(window).resize(function() {
+		$(".img-head").height($(".img-head").width())
+	})
+	$(".img-head").height($(".img-head").width())
+//	setInterval(function() {
+//		$(".img-head").css({
+//			"background": "url(../../img/avatars/4.pic_hd.jpg)",
+//			"background-size": "100% 100%"
+//		})
+//		setTimeout(function() {
+//			$(".img-head").css({
+//				"background": "url(../../img/avatars/2.pic_hd.jpg)",
+//				"background-size": "100% 100%"
+//			})
+//		}, 2000)
+//	}, 4000)
 	$(".back_skill").click(function() {
 		var a = $(this).next().next();
 		if ($(a).is(":visible")) {
 			$(this).next().removeClass('trans');
 			$(a).fadeOut();
-		}else{
+		} else {
 			$(this).next().addClass('trans');
 			$(".circle-desc").fadeOut();
 			$(a).fadeIn();
 		}
-		
+
 	})
 	$('.port-skills').waypoint(function(d) {
 		if (d == 'down') {
@@ -71,74 +42,74 @@ $(document).ready(function() {
 		offset: '85%',
 		triggerOnce: true,
 	});
-});
-$(".portfolio-item").find("[href='#']").click(function(e) {
-	e.preventDefault();
-});
-var selector = "#port-item-wrapper";
-$(function() {
-	var mix = $(selector).mixItUp({
-		animation: {
-			duration: 400,
-			effects: 'fade translateZ(-360px) stagger(34ms) translateX(50%) rotateY(50deg)',
-			easing: 'cubic-bezier(0.25, 0.46, 0.45, 0.94)'
-		},
-		selectors: {
-			target: '.portfolio-item',
-			filter: '.portfolio-item-filter'
-		}
+	$(".portfolio-item").find("[href='#']").click(function(e) {
+		e.preventDefault();
 	});
-});
-
-if ($.fn.owlCarousel) {
-	$(document).ready(function() {
-		$(".port-monials-slider").owlCarousel({
-			items: 3,
-			autoPlay: true,
-			pagination: false,
+	var selector = "#port-item-wrapper";
+	$(function() {
+		var mix = $(selector).mixItUp({
+			animation: {
+				duration: 400,
+				effects: 'fade translateZ(-360px) stagger(34ms) translateX(50%) rotateY(50deg)',
+				easing: 'cubic-bezier(0.25, 0.46, 0.45, 0.94)'
+			},
+			selectors: {
+				target: '.portfolio-item',
+				filter: '.portfolio-item-filter'
+			}
 		});
 	});
-}
 
-$(".nav-toggle").click(function() {
-	$("nav").toggleClass("active");
+	if ($.fn.owlCarousel) {
+		$(document).ready(function() {
+			$(".port-monials-slider").owlCarousel({
+				items: 3,
+				autoPlay: true,
+				pagination: false,
+			});
+		});
+	}
+
+	$(".nav-toggle").click(function() {
+		$("nav").toggleClass("active");
+	});
+	$(window).scroll(function() {
+		if ($(window).scrollTop() > 200 && $(window).innerWidth() <= 769)
+			$("nav").addClass('active-mobile');
+		else
+		if (!$("nav .navbar-collapse").hasClass('in'))
+			$("nav").removeClass('active-mobile');
+	});
+	$(window).resize(function() {
+		if ($(window).scrollTop() > 200 && $(window).innerWidth() <= 769)
+			$("nav").addClass('active-mobile');
+		else
+		if (!$("nav .navbar-collapse").hasClass('in'))
+			$("nav").removeClass('active-mobile');
+	});
+	$(".navbar-toggle").click(function() {
+		if ($(window).scrollTop() < 200 && $(window).innerWidth() <= 769)
+			$("nav").toggleClass('active-mobile');
+	});
+	$(".pangu").click(function() {
+		window.open("http://www.panguweb.cn/")
+	})
+	$(".jiuyou").click(function() {
+		window.open("http://www.pkey.cn/")
+	})
+	$(".wecheat").click(function() {
+		window.open("https://mumu1993.github.io/wechat/")
+	})
+	$(".dom").click(function() {
+		window.open("https://mumu1993.github.io/dom/")
+	})
+	$(".js").click(function() {
+		window.open("https://mumu1993.github.io/js-partice/")
+	})
+	$(".message").click(function() {
+		alert("正在建设中...请耐心等候")
+	})
+	$(".download").click(function() {
+		alert("正在建设中...请耐心等候")
+	})
 });
-$(window).scroll(function() {
-	if ($(window).scrollTop() > 200 && $(window).innerWidth() <= 769)
-		$("nav").addClass('active-mobile');
-	else
-	if (!$("nav .navbar-collapse").hasClass('in'))
-		$("nav").removeClass('active-mobile');
-});
-$(window).resize(function() {
-	if ($(window).scrollTop() > 200 && $(window).innerWidth() <= 769)
-		$("nav").addClass('active-mobile');
-	else
-	if (!$("nav .navbar-collapse").hasClass('in'))
-		$("nav").removeClass('active-mobile');
-});
-$(".navbar-toggle").click(function() {
-	if ($(window).scrollTop() < 200 && $(window).innerWidth() <= 769)
-		$("nav").toggleClass('active-mobile');
-});
-$(".pangu").click(function(){
-	window.open("http://www.panguweb.cn/")
-})
-$(".jiuyou").click(function(){
-	window.open("http://www.pkey.cn/")
-})
-$(".wecheat").click(function(){
-	window.open("https://mumu1993.github.io/wechat/")
-})
-$(".dom").click(function(){
-	window.open("https://mumu1993.github.io/dom/")
-})
-$(".js").click(function(){
-	window.open("https://mumu1993.github.io/js-partice/")
-})
-$(".message").click(function(){
-	alert("正在建设中...请耐心等候")
-})
-$(".download").click(function(){
-	alert("正在建设中...请耐心等候")
-})
